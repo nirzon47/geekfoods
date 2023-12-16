1f4e6c4cf05cc8611a5aefc920b845e73dc8a67c
Nirzon Karmakar <nirzon47@gmail.com>
2023-12-16 19:00:15 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './Header/Header'
 import Main from './Main/Main'
+import Footer from './Footer/Footer'
 
 const App = () => {
 	return (
 		<div className='relative'>
 			<Header />
 			<Main />
+			<Footer />
 		</div>
 	)
 }
